e9ef23a3d33576785d0c66805c4cdb8fa546cf5e
logonrm <logonrm@fiap.com.br>
2018-03-13 19:46:52 -0300

    Estrutura de controle

---
+++ Aula05.sql
@@ -0,0 +1,10 @@
+set SERVEROUTPUT ON;
+declare
+    v_idade number := &idade;
+begin 
+    if v_idade < 18 then
+        dbms_output.put_line('Menor de idade');
+    else 
+        dbms_output.put_line('Maior de idade');
+    end if;
+end;
